844b37ba59c733c7f8140d7503cb6f9924f91c89
julianp91 <juliancamilopenamanrique@gmail.com>
2024-03-15 22:46:42 -0500

    this is the login history

---
+++ functionschallenge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script Name:                  ops201-challenge03-functions
+# Author:                       julian pena
+# Date of latest revision:      03/15/2024
+# Purpose:                      prints logging history of user on this computer  
+
+# Declaration of variables
+
+
+# Declaration of functions
+print_the_login () {
+    last
+    echo "hi"
+    echo "this is my last loggings"
+}
+print_the_login
+print_the_login
+print_the_login
+# Main
+
+
+
+# End
